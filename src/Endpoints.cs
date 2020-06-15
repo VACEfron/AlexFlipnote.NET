73ff6f6bf4e7316eb783cff81139ecbd0946f58b
VACEfron <46462862+VACEfron@users.noreply.github.com>
2020-06-15 15:26:22 +0200

    v1.1

--- src/Endpoints.cs
+++ src/Endpoints.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System;
 using System.IO;
 
 namespace AlexFlipnote.NET
@@ -224,10 +225,17 @@ namespace AlexFlipnote.NET
         /// Returns an object with all provided color info.
         /// </summary>
         /// <returns></returns>
-        public static Color Color(string Hex)
+        public static Color Color(string Hex = "random")
         {
-            var data = RequestFunctions.JObjectRequest($"color/{Hex}");
+            if (Hex == "random")
+                Hex = string.Format("{0:X6}", new Random().Next(0x1000000));
 
+            var data = RequestFunctions.JObjectRequest($"color/{Hex}");
+            return GetColor(data);
+        }
+
+        private static Color GetColor(JObject data)
+        {
             var Color = new Color
             {
                 BlackOrWhiteText = data["blackorwhite_text"].Value<string>(),
@@ -291,70 +299,5 @@ namespace AlexFlipnote.NET
 
             return Steam;
         }
-    }
-
-    public class Color
-    {
-        public string BlackOrWhiteText { get; set; }
-        public int Brightness { get; set; }
-        public string Hex { get; set; }
-        public string ImageUrl { get; set; }
-        public string GradientImageUrl { get; set; }
-        public int Int { get; set; }
-        public string Name { get; set; }
-        public string RGB { get; set; }
-        public RgbValue RGBValue { get; set; }
-        public string[] Shades { get; set; }
-        public string[] Tints { get; set; }
-
-        public class RgbValue
-        {
-            public int R { get; set; }
-            public int G { get; set; }
-            public int B { get; set; }
-        }
-    }
-
-    public class Steam
-    {
-        public IDs SteamId { get; set; }
-        public SteamAvatar Avatar { get; set; }
-        public SteamProfile Profile { get; set; }
-
-        public class IDs
-        {
-            public string SteamId3 { get; set; }
-            public string SteamId32 { get; set; }
-            public string SteamId64 { get; set; }
-            public string CustomUrl { get; set; }
-        }
-        public class SteamAvatar
-        {
-            public string AvatarSmall { get; set; }
-            public string AvatarMedium { get; set; }
-            public string AvatarFull { get; set; }
-        }
-        public class SteamProfile
-        {
-            public string Username { get; set; }
-#nullable enable
-            public string? Realname { get; set; }
-#nullable disable
-            public string Url { get; set; }
-            public string Summary { get; set; }
-            public string Background { get; set; }
-            public string Location { get; set; }
-            public string Status { get; set; }
-            public string Privacy { get; set; }
-            public string TimeCreated { get; set; }
-            public bool VACBanned { get; set; }
-        }
-    }
-
-    public enum LogoType
-    {
-        Normal,
-        Dark,
-        Light
-    }
+    }    
 }
---
+++ src/Objects.cs
@@ -0,0 +1,67 @@
+﻿namespace AlexFlipnote.NET
+{
+    public class Color
+    {
+        public string BlackOrWhiteText { get; set; }
+        public int Brightness { get; set; }
+        public string Hex { get; set; }
+        public string ImageUrl { get; set; }
+        public string GradientImageUrl { get; set; }
+        public int Int { get; set; }
+        public string Name { get; set; }
+        public string RGB { get; set; }
+        public RgbValue RGBValue { get; set; }
+        public string[] Shades { get; set; }
+        public string[] Tints { get; set; }
+
+        public class RgbValue
+        {
+            public int R { get; set; }
+            public int G { get; set; }
+            public int B { get; set; }
+        }
+    }
+
+    public class Steam
+    {
+        public IDs SteamId { get; set; }
+        public SteamAvatar Avatar { get; set; }
+        public SteamProfile Profile { get; set; }
+
+        public class IDs
+        {
+            public string SteamId3 { get; set; }
+            public string SteamId32 { get; set; }
+            public string SteamId64 { get; set; }
+            public string CustomUrl { get; set; }
+        }
+        public class SteamAvatar
+        {
+            public string AvatarSmall { get; set; }
+            public string AvatarMedium { get; set; }
+            public string AvatarFull { get; set; }
+        }
+        public class SteamProfile
+        {
+            public string Username { get; set; }
+#nullable enable
+            public string? Realname { get; set; }
+#nullable disable
+            public string Url { get; set; }
+            public string Summary { get; set; }
+            public string Background { get; set; }
+            public string Location { get; set; }
+            public string Status { get; set; }
+            public string Privacy { get; set; }
+            public string TimeCreated { get; set; }
+            public bool VACBanned { get; set; }
+        }
+    }
+
+    public enum LogoType
+    {
+        Normal,
+        Dark,
+        Light
+    }
+}
