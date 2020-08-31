489b792d6228d122e3f8b895c4ce3747618dac78
thunderstorm010 <yigitozdemir1299@gmail.com>
2020-08-31 18:31:58 +0300

    Fix

---
+++ src/RequestFunctions.cs
@@ -0,0 +1,65 @@
+using System;
+using System.IO;
+using System.Net.Http;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AlexFlipnote.NET
+{
+    public static class RequestFunctions
+    {
+        public static string JsonRequest(string endpoint, string jsonObject)
+        {
+            try
+            {
+                JObject data = MakeWebRequest(endpoint);
+
+                return data[jsonObject].Value<string>();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static JObject JObjectRequest(string endpoint)
+        {
+            try
+            {
+                return MakeWebRequest(endpoint);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static MemoryStream ImageRequest(string endpoint)
+        {
+            using var httpClient = new HttpClient();
+            var response = httpClient.GetAsync("https://api.alexflipnote.dev/" + endpoint,
+                HttpCompletionOption.ResponseContentRead);
+            var responseMessage = response.Result;
+            if (!responseMessage.IsSuccessStatusCode)
+            {
+                var result = responseMessage.Content.ReadAsStringAsync().Result;
+                var htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(result);
+                var responseText = htmlDoc.DocumentNode.LastChild.InnerText.Replace("\n","");
+                throw new Exception($"Status {(int)responseMessage.StatusCode}: {responseText}");
+            }
+            
+
+            var stream = responseMessage.Content.ReadAsStreamAsync();
+            return (MemoryStream) stream.Result;
+        }
+
+        public static JObject MakeWebRequest(string endpoint)
+        {
+            var request = new HttpClient();
+            var httpResponseMsg = request.GetAsync($"https://api.alexflipnote.dev/{endpoint}");
+            return (JObject) JsonConvert.DeserializeObject(httpResponseMsg.Result.Content.ReadAsStringAsync().Result);
+        }
+    }
+}
